spice-gm/spice-streaming-agent

commit e0cf764baff9d678a6e75916457300d1ff39b169

gst-plugin: Use autovideoconvert element for color space conversion
When VAAPI based encoder is used it's likely that also VAAPI based
conversion is available (using the vaapipostproc element).
Moving to autovideoconvert will automatically choose a suitable
convert element according to elements availability (e.g. in the
VAAPI case it would select vaapipostproc due to its high rank).

Signed-off-by: Snir Sheriber <ssheribe@redhat.com>
Acked-by: Frediano Ziglio <fziglio@redhat.com>

src/gst-plugin.cpp
@@ -216,9 +216,9 @@ void GstreamerFrameCapture::pipeline_init(const GstreamerEncoderSettings &settin
     if (!capture) {
         throw std::runtime_error("Gstreamer's capture element cannot be created");
     }
-    GstObjectUPtr<GstElement> convert(gst_element_factory_make("videoconvert", "convert"));
+    GstObjectUPtr<GstElement> convert(gst_element_factory_make("autovideoconvert", "convert"));
     if (!convert) {
-        throw std::runtime_error("Gstreamer's 'videoconvert' element cannot be created");
+        throw std::runtime_error("Gstreamer's 'autovideoconvert' element cannot be created");
     }
     GstCapsUPtr sink_caps;
     GstObjectUPtr<GstElement> encoder(get_encoder_plugin(settings, sink_caps));

commit 36410d58413b91353ce326ff33aa82954061a659

gst-plugin: Changing framerate crash when ximagesrc is used
When XLIB_CAPTURE is set to 0 and framerate is set to anything
other than 25 streaming-agent crashes

This is a regression caused by e0cf764baff9d678a6e75916457300d1ff39b169
Fixing it is done by adding the framerate value to conversion caps

Signed-off-by: Snir Sheriber <ssheribe@redhat.com>
Acked-by: Frediano Ziglio <fziglio@redhat.com>

src/gst-plugin.cpp
@@ -237,7 +237,10 @@ void GstreamerFrameCapture::pipeline_init(const GstreamerEncoderSettings &settin
     gst_bin_add(bin, sink);
 
     GstCapsUPtr caps(gst_caps_from_string("video/x-raw(ANY)"));
-    link = gst_element_link(capture.get(), convert.get()) &&
+    GstCapsUPtr convert_caps(gst_caps_new_simple("video/x-raw",
+                                                 "framerate", GST_TYPE_FRACTION, settings.fps, 1,
+                                                 nullptr));
+    link = gst_element_link_filtered(capture.get(), convert.get(), convert_caps.get()) &&
            gst_element_link_filtered(convert.get(), encoder.get(), caps.get()) &&
            gst_element_link_filtered(encoder.get(), sink.get(), sink_caps.get());
     if (!link) {